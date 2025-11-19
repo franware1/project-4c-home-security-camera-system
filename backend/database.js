12b461a2ec85ca1811b470ff0066734908a6a910
Francisco Vu <franvu225@outlook.com>
2025-11-19 18:38:02 -0500

    Using BrowserRouter to change pages
    Will likely have to change the structure of the main.tsx or App.tsx so that we can switch from page to page

--- backend/database.js
+++ backend/database.js
@@ -2,8 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const app = express();
+const mongoose = require("mongoose");
+PORT = 5000;  
 
-function fetchUser (username) {
-  if (!username) return ''
-  return 
+DB_URL = 'mongodb://localhost:27017/mydatabase';
+mongoose.connect(DB_URL);
+const conn = mongoose.connection;
+
+conn.once('open', () => {
+  console.log('Database connected successfully');
+});
+
+app.use(cors());
+app.use(express.json());
+
+export function fetchUser (username) {
+  if (!username) return '';
+  // fetch user from database
+
+  
 }
--- frontend/src/entry_page/signin.tsx
+++ frontend/src/entry_page/signin.tsx
@@ -1,6 +1,7 @@
 import React, { FormEvent, useEffect, useState } from "react";
 import ReactDom, { useFormStatus } from "react-dom";
-import { validate } from "../../../backend/database.js"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // use this to route user to different pages
+import { fetchUser } from "../../../backend/database.js"
 
 export default function SignInPage() {
 
@@ -8,43 +9,65 @@ export default function SignInPage() {
     const [password, setPassword] = useState<string>("");
 
     function processUser(userSignInAttempt: React.FormEvent<HTMLFormElement>) {
+      userSignInAttempt.preventDefault(); // prevent page reload on form submit
+
       console.log("User sign-in attempt")
       console.log("Submitting, ", {username, password})
+
       'use server'
-      validate{username, password}
+      const user = fetchUser(username);
+
+      if (!user) {
+        console.log("User not found in database")
+      }
+      else {
+        console.log("User found in database:", user)
+      }
 
     } 
 
     
   return (
-    <form onSubmit={processUser}>
+    <div className='signin-container'>
+      <form className="signin-form" onSubmit={processUser}>
 
-      <div>
-        <label>
-          Username:
-        <input
-          id="username"
-          type="email"
-          value={username}
-          onChange={(user_typed_username) => setUsername(user_typed_username.target.value)} // inline function to change username on screen
-        />
-        </label>
+        <div className="form-box">
+          <label>
+            Email address:
+          <input
+            id="email"
+            type="email"
+            value={username}
+            onChange={(user_typed_username) => setUsername(user_typed_username.target.value)} // inline function to change username on screen
+          />
+          </label>
 
-        <label>Password:
-        <input
-          id="password"
-          value={password}
-          onChange={(user_typed_password) => setPassword(user_typed_password.target.value)} // inline function to change password on screen
-        />
-        </label>
+          <label>Password:
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(user_typed_password) => setPassword(user_typed_password.target.value)} // inline function to change password on screen
+          />
+          </label>
 
-        <button
-          type="submit"
-          title="Sign In"
-          color="#0e0d6eff"
-        />
+          <button
+            type="submit"
+            title="Sign In"
+            color="#0e0d6eff"
+          />
+        </div>
 
-      </div>
-    </form>
+        <div className="forgot-password-link">
+          <Route path="/reset-password"/>
+        </div>
+
+        <div className="signup-link">
+          Don't have an account?
+          <a href="/signup.tsx">Sign up</a>
+        </div>
+
+      </form>
+    </div>
   );
 }
